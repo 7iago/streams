3d0fe779218b33d09dc241a0bd3845ee342d0a2c
Tiago <74260711+7iago@users.noreply.github.com>
2021-03-19 16:50:34 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -3,10 +3,12 @@ package streams;
 public class Aluno {
 	final String nome;
 	final double nota;
+	final boolean bomCoportamento;
 	
 	public Aluno(String nome, double nota) {
 		this.nome = nome;
 		this.nota = nota;
+		this.bomCoportamento = bom
 	}
 	
 	
