3ef99148bae16068ff5ac82e380af1777e7bae1e
Tiago <74260711+7iago@users.noreply.github.com>
2021-03-24 11:58:07 -0300

    stream api completo

--- Filter.java
+++ Filter.java
@@ -10,21 +10,23 @@ public class Filter {
 
 	public static void main(String[] args) {
 		
-		Aluno a1 = new Aluno("Ana", 7.8);
-		Aluno a2 = new Aluno("Bia", 5.8);
-		Aluno a3 = new Aluno("Daniel", 9.8);
-		Aluno a4 = new Aluno("Gui", 6.8);
-		Aluno a5 = new Aluno("Rebeca", 7.1);
-		Aluno a6 = new Aluno("Pedro", 8.8);
+		Aluno a1 = new Aluno("Ana", 7.8, false);
+		Aluno a2 = new Aluno("Bia", 5.8, true);
+		Aluno a3 = new Aluno("Daniel", 9.8, true);
+		Aluno a4 = new Aluno("Gui", 6.8, true);
+		Aluno a5 = new Aluno("Rebeca", 7.1, true);
+		Aluno a6 = new Aluno("Pedro", 8.8, false);
 		
 		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
 		
 		Predicate<Aluno> aprovado = a -> a.nota >= 7;
+		Predicate<Aluno> comportamento = a -> a.bomCoportamento;
 		Function<Aluno, String> saudacaoAprovado = 
-				a -> "Parabens " + a.nome + " você foi aprovado!";
+				a -> "Parabens " + a.nome + " vocÃª foi aprovado!";
 		
 		alunos.stream()
 			.filter(aprovado)
+			.filter(comportamento)
 			.map(saudacaoAprovado)
 			.forEach(System.out::println);
 	}
